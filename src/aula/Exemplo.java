c83a9293e1477b155b8aa660d73fb358ba575a05
almeidinhaa <gustavo_almeida20@yahoo.com.br>
2019-06-12 12:34:22 -0300

    primeiro fucking upload

---
+++ src/aula/Exemplo.java
@@ -0,0 +1,30 @@
+package aula;
+
+public class Exemplo {
+
+	public double adicao(double n, double n1) {
+		return n + n1;
+	}
+	
+	public double subtracao(double n, double n1) {
+		return n - n1;
+	}
+	
+	public double multiplicacao(double n, double n1) {
+		return n * n1;
+	}
+	
+	public double divisao(double n, double n1) {
+		return n / n1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exemplo ex = new Exemplo();
+		
+		System.out.println(ex.adicao(10, 1));
+		System.out.println(ex.subtracao(10, 5));
+		System.out.println(ex.multiplicacao(6, 4));
+		System.out.println(ex.divisao(8, 2));
+	}
+}
